2caad39135fe227f04050a00617f9ecf6812acef
gjjaureg <146881856+gjjaureg@users.noreply.github.com>
2024-05-16 06:50:57 -0700

    platformer 2

---
+++ src/Scenes/Load2.js
@@ -0,0 +1,55 @@
+class Load2 extends Phaser.Scene {
+    constructor() {
+        super("loadScene2");
+    }
+
+    preload() {
+        this.load.setPath("./assets/");
+
+        // Load characters spritesheet
+        this.load.atlas("platformer_characters", "tilemap-characters-packed.png", "tilemap-characters-packed.json");
+
+        // Load tilemap information
+        this.load.image("tilemap_tiles_food", "tilemap_packed_food.png");                         // Packed tilemap
+        this.load.tilemapTiledJSON("platformer-level-2", "platformer-level-2.tmj");   // Tilemap in JSON
+    }
+
+    create() {
+        this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNames('platformer_characters', {
+                prefix: "tile_",
+                start: 0,
+                end: 1,
+                suffix: ".png",
+                zeroPad: 4
+            }),
+            frameRate: 15,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'idle',
+            defaultTextureKey: "platformer_characters",
+            frames: [
+                { frame: "tile_0000.png" }
+            ],
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'jump',
+            defaultTextureKey: "platformer_characters",
+            frames: [
+                { frame: "tile_0001.png" }
+            ],
+        });
+
+         // ...and pass to the next Scene
+         this.scene.start("platformerScene2");
+    }
+
+    // Never get here since a new scene is started in create()
+    update() {
+    }
+}
